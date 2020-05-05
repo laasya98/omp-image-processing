da3fde2efb41a3c5030e1be5133a2f9acee9f69a
Laasya Renganathan <lpr46@cornell.edu>
2020-05-05 00:52:58 -0400

    loading bimps

--- load_bmp.c
+++ load_bmp.c
@@ -3,9 +3,9 @@
 
 #include "bmp.h"
 
-unsigned char *read_bmp(char *fname, BITMAPINFOHEADER bi, unsigned char *img, int padding)
+void read_bmp(FILE *fname, BITMAPINFOHEADER bi, unsigned char *img, int padding)
 {
-    int img_idx = 0
+    int img_idx = 0;
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
@@ -16,65 +16,62 @@ unsigned char *read_bmp(char *fname, BITMAPINFOHEADER bi, unsigned char *img, in
             RGBTRIPLE triple;
 
             // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+            fread(&triple, sizeof(RGBTRIPLE), 1, fname);
+            //printf("%x, %x, %x\n", triple.rgbtBlue, triple.rgbtGreen, triple.rgbtRed);
 
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            // put into char array (BGR order)
+            img[img_idx] = .3*triple.rgbtBlue + .59*triple.rgbtGreen + .11*triple.rgbtRed;
+            img_idx ++;
+
         }
-
         // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
-
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
-        }
+        fseek(fname, padding, SEEK_CUR);
     }
 }
 
-void write_bmp(char *fname, int size, unsigned char *header, unsigned char *img)
+void write_bmp(FILE *fname, BITMAPINFOHEADER bi, unsigned char *img, int padding)
 {
-    int img_idx = 0
+    int img_idx = 0;
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
         // iterate over pixels in scanline
         for (int j = 0; j < bi.biWidth; j++)
         {
-            // temporary storage
-            RGBTRIPLE triple;
-
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
             // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            unsigned char r, g, b, gray;
+            gray = img[img_idx];
+            r = gray/.3;
+            g = gray/.59;
+            b = gray/.11; 
+            fputc(r, fname);
+            fputc(g, fname);
+            fputc(b, fname);
+            img_idx ++;
         }
-
-        // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
 
         // then add it back (to demonstrate how)
         for (int k = 0; k < padding; k++)
         {
-            fputc(0x00, outptr);
+            fputc(0x00, fname);
         }
     }
+    printf("%d\n", img_idx);
 }
 
 // perform simple binary threshold
-void thresh(unsigned char *img, unsigned char *edited_img, size, thresh) 
+void threshold(unsigned char *img, unsigned char *edited_img, BITMAPINFOHEADER bi, int thresh) 
 {
-    for (int i; i < size; i++) {
-        for (int j; j < size; j++) {
-            if (img[i][j] < thresh) {
-                edited_img[i][j] = 0;
-            }
-            else {
-                edited_img[i][j] = 255;
-            }
+    for (int i = 0; i < (bi.biHeight * bi.biWidth); i++) {
+        if (img[i] < thresh) {
+            edited_img[i] = 0x00;
+            //printf("%d, %d | ",  img[i], edited_img[i]);
         }
+        else {
+            edited_img[i] = 0xFF;
+            //printf("%d, %d | ",  img[i], edited_img[i]);
+        }
+
     }
 }
 
@@ -85,10 +82,8 @@ int main(int argc, char* argv[])
     char* infile = argv[1];
     char* outfile = argv[2];
     // operation to perform
-    int mode = argv[3];
+    int mode = atoi(argv[3]);
     
-    
-
     FILE *source = fopen(infile, "rb+");
     FILE *dest = fopen(outfile, "wb+");
     
@@ -96,32 +91,36 @@ int main(int argc, char* argv[])
     // output file and info headers will be the same
     BITMAPFILEHEADER bf;
     BITMAPINFOHEADER bi;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, source);
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, source);
+    
+    // write outfile's BITMAPFILEHEADER and BITMAPINFOHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, dest);
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, dest);
 
     // init image buffers
-    unsigned char img[bi.biWidth * bi.biHeight * 3];
-    unsigned char edited_img[bi.biWidth * bi.biHeight * 3];
+    unsigned char img[bi.biWidth * bi.biHeight];
+    unsigned char edited_img[bi.biWidth * bi.biHeight];
 
     // determine padding for scanlines
     int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     // read infile into char array
-    read_bmp(source, img);
+    read_bmp(source, bi, img, padding);
 
     // TODO: add a case/switch here for different modes of operation
     // TODO: OMP declarations
 
     //  binary thresh for now
-    thresh(img, edited_img, bi, 128);
+    threshold(img, edited_img, bi, 128);
 
-    write_bmp("dest.bmp", size, header, img);
+    write_bmp(dest, bi, edited_img, padding);
 
     // close infile
-    fclose(inptr);
+    fclose(source);
 
     // close outfile
-    fclose(outptr);
+    fclose(dest);
 
     return 0;
 }
