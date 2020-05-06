8b413413df07f61701b4b43f79c0c56e9312b204
David Pirogovsky <dap263@cornell.edu>
2020-05-06 15:30:41 -0400

    run over all .bmp in /images for seq and omp, output to input.csv

---
+++ all.sh
@@ -0,0 +1,21 @@
+if [ $# -ne 1 ]
+then 
+	echo "please enter <out.csv>"
+	exit
+fi
+
+echo "name, height, width, time, numRuns, p" > $1
+
+array=(1 2 4 8)
+
+FILES=$(ls images/*.bmp)
+for f in $FILES
+do 
+	echo $f
+	./run.sh $f out.bmp 0 0 $1
+	for n in "${array[@]}"
+	do
+		echo $n
+		./omp.sh $f out.bmp 0 $n 0 $1
+	done
+done
